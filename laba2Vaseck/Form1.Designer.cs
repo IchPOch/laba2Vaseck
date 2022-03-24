f7280ea1ea4658117dde6c4a0af6f822e3f4b6aa
Михаил <@DESKTOP-3P2543V>
2022-03-24 16:15:42 +0300

    Добавьте файлы проекта.

---
+++ laba2Vaseck/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace laba2Vaseck
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Посчитать формулы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 282);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 146);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(776, 237);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ laba2Vaseck/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+namespace laba2Vaseck
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            String Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            textBox1.Text = "Version: " + Version;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Pvp(double T)
+                {
+                double A = 14.9571;
+                double B = 3231.225;
+                double C = -98.138;
+                double O = Math.Exp(A - B / (T + C));
+                return O;
+                }
+            double Pvpr(double T)
+            {
+                double a = -11.806;
+                double b = 12.0699;
+                double c = -20.477;
+                double d = 13.884;
+                double Tc = 588.1;
+                double Pc = 3.897;
+                double Tg = T / Tc;
+                double g = 1 - Tg;
+                double O = Math.Exp((1/Tg)*(a*g + b * Math.Pow(g,1.5) + c * Math.Pow(g,2.5) + d * Math.Pow(g,5)))*Pc;
+                return O;
+            }
+            
+            int i = 318;
+            int s = 0;
+            double[] T = new double[18];
+            while (i <= 403)
+            {
+                T[s] = i;
+                s++;
+                i += 5;
+            }
+            double[] Ant = new double[18];
+            double[] Vag = new double[18];
+            for (int j = 0; j < 18; j++)
+            {
+                Ant[j] = Pvp(T[j]);
+                Vag[j] = Pvpr(T[j]);
+                textBox2.Text += "Температура: " + T[j].ToString() + " Формула Антуана: " + Ant[j].ToString() + " Формула Вагнера: " + Vag[j].ToString() + " Точность: " + (Ant[j] - Vag[j]).ToString() + Environment.NewLine;
+            }
+
+
+        }
+    }
+}
